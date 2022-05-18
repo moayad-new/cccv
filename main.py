a8fbcdc0b30eb30591d4d8ccc676a891edc22f18
moayad-new <105077020+moayad-new@users.noreply.github.com>
2022-05-18 10:48:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os
-os.system("git clone https://github.com/noayad/AKSJSJDJ")
+os.system("git clone https://github.com/moayad-new/cccv?organization=moayad-new&organization=moayad-new")
 os.system("cd Hack && python bot.py")
